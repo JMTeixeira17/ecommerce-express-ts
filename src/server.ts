eb3c7906377be12ac32837df03d625d8e17aa6dd
José Manuel <49705208+JMTeixeira17@users.noreply.github.com>
2025-02-02 16:13:06 -0400

    Delete middleware on server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app._router.stack.forEach((middleware: any) => {
-  if (middleware.route) {
-    console.log(`${Object.keys(middleware.route.methods)} -> ${middleware.route.path}`);
-  } else if (middleware.name === 'router') {
-    middleware.handle.stack.forEach((handler: any) => {
-      if (handler.route) {
-        console.log(`${Object.keys(handler.route.methods)} -> ${handler.route.path}`);
-      }
-    });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
